7a3c04051a358bf0867413e8b3af9dd6672a5f37
eharjes <elias@visiolab.io>
2024-02-22 02:28:45 +0100

    server-setup

--- channel.py
+++ channel.py
@@ -17,9 +17,10 @@ app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigClass')  # configuration
 app.app_context().push()  # create an app context before initializing db
 
-HUB_URL = 'http://localhost:5555'
+HUB_URL =  "https://temporary-server.de" # 'http://localhost:5555'
 HUB_AUTHKEY = '1234567890'
 CHANNEL_AUTHKEY = '0987654321'
+SERVER_AUTHKEY = 'Crr-K3d-2N'
 CHANNEL_NAME = "The One and Only Channel"
 CHANNEL_ENDPOINT = "http://localhost:5001" # don't forget to adjust in the bottom of the file
 CHANNEL_FILE = 'messages.json'
